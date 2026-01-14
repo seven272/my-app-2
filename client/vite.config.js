6738458fe4cba6742854fac8c60fb18b94d7404d
seven272 <i727i@mail.ru>
2026-01-14 13:15:28 +0300

    first commit

---
+++ client/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173, // This is the port which we will use in docker
+    allowedHosts: ['prank-sound.ru'],
+    strictPort: true,
+    // Thanks @sergiomoura for the window fix
+    // add the next lines if you're using windows and hot reload doesn't work
+    watch: {
+      usePolling: true,
+    },
+  },
+  preview: {
+    port: 4500,
+    strictPort: true,
+  },
+})
